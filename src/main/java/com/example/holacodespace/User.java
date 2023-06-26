bc9c698a2fcbd0f661019d2e7e55cd35f7f83e55
CapocaG <134323943+CapocaG@users.noreply.github.com>
2023-06-26 19:05:46 +0000

    listo.

--- src/main/java/com/example/holacodespace/User.java
+++ src/main/java/com/example/holacodespace/User.java
@@ -4,10 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+//import jakarta.persistence.Table;
 
 @Entity 
-@Tabla(name = "usuario")
+//@Tabla(name = "usuario")
 public class User {
     @Id 
 	@GeneratedValue(strategy=GenerationType.AUTO)
